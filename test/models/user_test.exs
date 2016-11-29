7af43e8e296311aa59c025dc87ed97ec6ab5a5b4
Scott Batson <sjbats@email.com>
2016-11-29 15:31:31 -0500

    add more test coverage

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -19,6 +19,22 @@ defmodule MovieWagerApi.UserTest do
   test "`by_id_or_username` queries by id when passed an integer" do
     user = insert(:user, username: "wehavefun")
     query = User.by_id_or_username(user.id)
+
     assert Repo.one(query).username == user.username
+  end
+
+  test "`by_id_or_username` queries by string id's" do
+    user = insert(:user, id: 5)
+    query = User.by_id_or_username("5")
+
+    assert Repo.one(query).id == 5
+  end
+
+  test "by_id_or_username` queries by username when passed" do
+    username = "scottisawesome"
+    user = insert(:user, username: username)
+    query = User.by_id_or_username(username)
+
+    assert Repo.one(query).id == user.id
   end
 end
